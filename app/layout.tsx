3efaeab1aa281cd8d19b282d8f709a68198dbb3e
helen-akateva <akateva17@gmail.com>
2026-05-04 12:37:01 +0300

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,19 +23,22 @@ export default function RootLayout({
   return (
     <html lang="uk">
       <head>
+        {/* Google Tag Manager */}
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id=GTM-W9TD9DPM'+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W9TD9DPM');`,
           }}
         />
       </head>
+
       <body className={geist.className}>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W9TD9DPM"
@@ -44,6 +47,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+
         {children}
       </body>
     </html>
